8fc99f4fd4012d5f31d05a27bd85c810e0add28b
rn4ir <rn@rahul-nair.com>
2018-01-24 20:19:58 -0500

    added check-IP function

--- pymyip.py
+++ pymyip.py
@@ -7,6 +7,27 @@ Requirements:
 """
 import sys
 import argparse
+import urllib.request, json
+
+def myip(*arg):
+    """
+    Function to get the WAN IP's information.
+    """
+
+    ip_response =  json.loads(urllib.request.urlopen('http://wtfismyip.com/json').read().decode("utf-8"))
+    
+    if (arg):
+        if (arg[0] == "l"):
+            print ("\nYour WAN IP's location is: " + ip_response["YourFuckingLocation"] + "\n")
+        elif (arg[0] == "a"):
+            print ("\nYour WAN IP is:\t\t\t" + ip_response["YourFuckingIPAddress"])
+            print ("Your WAN IP's hostname is:\t" + ip_response["YourFuckingHostname"])
+            print ("Your WAN IP's ISP-ASN is:\t\t" + ip_response["YourFuckingISP"])
+            print ("Your WAN IP's location is:\t" + ip_response["YourFuckingLocation"] + "\n")
+        else:
+            print ("Wrong argument!")
+    else:
+        return ip_response["YourFuckingIPAddress"]
 
 def main():
     """
@@ -15,17 +36,17 @@ def main():
     """
 
     cli_argparser = argparse.ArgumentParser(description='')
-    cli_argparser.add_argument('-l', '--all', nargs='?', const=1, help="Lists all the information about the current WAN IP.", required=False)
-    cli_argparser.add_argument('-c', '--country', nargs='?', const=1, help="Lists the country of origin for the current WAN IP.", required=False)
+    cli_argparser.add_argument('-a', '--all', nargs='?', const=1, help="Lists all the information about the current WAN IP.", required=False)
+    cli_argparser.add_argument('-l', '--location', nargs='?', const=1, help="Lists the country of origin for the current WAN IP.", required=False)
 
     cli_args = cli_argparser.parse_args()
 
     if (cli_args.all):
-        print ("All")
-    elif (cli_args.country):
-        print ("Country")
+        (myip("a"))
+    elif (cli_args.location):
+        (myip("l"))
     else:
-        print ("Default")
+        print ("Your WAN IP is: " + myip())
 
 if __name__ == '__main__':
     sys.exit(main())
