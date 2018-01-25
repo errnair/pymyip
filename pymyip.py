a410ff56a0915fd46331998f78afe6fe95a7bdad
rn4ir <rn@rahul-nair.com>
2018-01-24 19:39:13 -0500

    added argparse options

--- pymyip.py
+++ pymyip.py
@@ -5,6 +5,7 @@ A python script to check Alexa rankings of websites.
 Requirements:
 - Python 3.x
 """
+import sys
 import argparse
 
 def main():
@@ -14,9 +15,17 @@ def main():
     """
 
     cli_argparser = argparse.ArgumentParser(description='')
-    cli_argparser.add_argument('-l', '--all', help="Lists all the information about the current WAN IP.", required=False)
-    cli_argparser.add_argument('-c', '--country', help="Lists the country of origin for the current WAN IP.", required=False)
+    cli_argparser.add_argument('-l', '--all', nargs='?', const=1, help="Lists all the information about the current WAN IP.", required=False)
+    cli_argparser.add_argument('-c', '--country', nargs='?', const=1, help="Lists the country of origin for the current WAN IP.", required=False)
+
     cli_args = cli_argparser.parse_args()
+
+    if (cli_args.all):
+        print ("All")
+    elif (cli_args.country):
+        print ("Country")
+    else:
+        print ("Default")
 
 if __name__ == '__main__':
     sys.exit(main())
